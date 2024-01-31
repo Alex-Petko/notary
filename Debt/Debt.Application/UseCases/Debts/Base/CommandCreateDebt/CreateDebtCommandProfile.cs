c97346b60c7728a78b9bde16283a63b371b48c27
Alexander Petko <petko.alexander.work@gmail.com>
2024-01-31 15:20:24 +0300

    chore(debt.application): slice debtMapper for greater cohesion

---
+++ Debt/Debt.Application/UseCases/Debts/Base/CommandCreateDebt/CreateDebtCommandProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DebtManager.Domain;
+
+namespace DebtManager.Application;
+
+internal sealed class CreateDebtCommandProfile : Profile
+{
+    public CreateDebtCommandProfile()
+    {
+        CreateMap<CreateDebtCommand, Debt>()
+            .IncludeMembers(x => x.Body);
+
+        CreateMap<CreateDebtCommandBody, Debt>();
+    }
+}
---
+++ Debt/Debt.Application/UseCases/Debts/QueryGetDebt/GetDebtQueryResultProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using DebtManager.Domain;
+
+namespace DebtManager.Application;
+
+internal sealed class GetDebtQueryResultProfile : Profile
+{
+    public GetDebtQueryResultProfile()
+    {
+        CreateMap<Debt, GetDebtQueryResult>();
+    }
+}
