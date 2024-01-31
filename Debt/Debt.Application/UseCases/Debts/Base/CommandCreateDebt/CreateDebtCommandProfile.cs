12abfdbcbd9ed139c1e26eb7c52a72789a6196c3
Alexander Petko <petko.alexander.work@gmail.com>
2024-01-31 15:37:35 +0300

    feat(debt.tests) add CreateDebtCommandProfileTests

--- Debt/Debt.Application/UseCases/Debts/Base/CommandCreateDebt/CreateDebtCommandProfile.cs
+++ Debt/Debt.Application/UseCases/Debts/Base/CommandCreateDebt/CreateDebtCommandProfile.cs
@@ -8,8 +8,17 @@ internal sealed class CreateDebtCommandProfile : Profile
     public CreateDebtCommandProfile()
     {
         CreateMap<CreateDebtCommand, Debt>()
+            .ForMember(x => x.Id, opt => opt.Ignore())
+            .ForMember(x => x.LenderLogin, opt => opt.Ignore())
+            .ForMember(x => x.BorrowerLogin, opt => opt.Ignore())
+            .ForMember(x => x.Status, opt => opt.Ignore())
+            .ForSourceMember(x => x.Login, opt => opt.DoNotValidate())
             .IncludeMembers(x => x.Body);
 
-        CreateMap<CreateDebtCommandBody, Debt>();
+        CreateMap<CreateDebtCommandBody, Debt>()
+            .ForMember(x => x.Id, opt => opt.Ignore())
+             .ForMember(x => x.LenderLogin, opt => opt.Ignore())
+             .ForMember(x => x.BorrowerLogin, opt => opt.Ignore())
+             .ForMember(x => x.Status, opt => opt.Ignore());
     }
 }
---
+++ Debt/Debt.Tests/Debt.Application/Debts/UseCases/Base/CommandCreateDebt/CreateDebtCommandProfileTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DebtManager.Application;
+
+namespace Application.Profiles;
+
+public class CreateDebtCommandProfileTests
+{
+    [Fact]
+    public void AutoMapperTest()
+    {
+        // Arrange
+        var configuration = new MapperConfiguration(configure => configure.AddProfile<CreateDebtCommandProfile>());
+
+        // Act
+        // Assert
+        configuration.AssertConfigurationIsValid();
+    }
+}
