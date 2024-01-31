7bbaad2f4559224d5d2033f647b4fe7ff5cb3534
Alexander Petko <petko.alexander.work@gmail.com>
2024-01-31 15:29:32 +0300

    feat(debt.tests) add GetDebtQueryResultProfileTests

---
+++ Debt/Debt.Tests/Debt.Application/Debts/UseCases/QueryGetDebt/GetDebtQueryResultProfileTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DebtManager.Application;
+
+namespace Application.Profiles;
+
+public class GetDebtQueryResultProfileTests
+{
+    [Fact]
+    public void AutoMapperTest()
+    {
+        // Arrange
+        var configuration = new MapperConfiguration(configure => configure.AddProfile<GetDebtQueryResultProfile>());
+
+        // Act
+        // Assert
+        configuration.AssertConfigurationIsValid();
+    }
+}
